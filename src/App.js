55fde90b5ae1ce20ba64e0a00773591c3191d379
Alexnhlst <agostinacchioalessandro@gmail.com>
2023-07-22 20:43:43 +0200

    Created the header component

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/profile" element={<Profile />}></Route>
           <Route path="/sign-in" element={<SignIn />}></Route>
           <Route path="/sign-up" element={<SignUp />}></Route>
           <Route path="/forgot-password" element={<ForgotPassword />}></Route>
-          <Route path="/offersgit" element={<Offers />}></Route>
+          <Route path="/offers" element={<Offers />}></Route>
         </Routes>
       </Router>
     </>
